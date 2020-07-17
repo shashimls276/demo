af38f01f1b0ccd510e2c6508ade381cc9fa5b9df
ab68478 <ab68478@IND-5CG9381KGD.ctl.intranet>
2020-07-17 20:35:10 +0530

    Adding Controller Files

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ab68478
+ *
+ */
+@RestController
+public class Controller {
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World CI/CD";
+	}
+}
